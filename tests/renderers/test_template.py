596d01b276b9022cd6a7091cfb22af0fcba8c742
Paris Kolios <paris.k@gmail.com>
2017-08-30 14:52:18 +0200

    Fix renderer tests for new site fixture

--- tests/renderers/test_template.py
+++ tests/renderers/test_template.py
@@ -1,5 +1,8 @@
 from unittest import mock
 
+import pytest
+
+from mackerel import exceptions
 from mackerel.renderers import template
 
 
@@ -15,9 +18,30 @@ class TestJinja2Renderer:
         jinja2.Environment.assert_called_with(
             loader=mock.ANY, lstrip_blocks=True, trim_blocks=True)
 
-    def test_render(self, site, document, context):
+    def test_render(self, site, document_mocks):
+        document = document_mocks.create(template='path/to/template')
+        context = mock.Mock('context')
+
         renderer = template.Jinja2Renderer(site=site)
-        html = renderer.render(ctx=context, document=document)
-        assert '<!DOCTYPE html>' in html
-        assert '<a href="/about.html">About</a>' in html
-        assert '<h1 class="post-title">About</h1>' in html
+        render_func = mock.Mock()
+        renderer.env.get_template = mock.Mock(
+            return_value=mock.Mock(render=render_func))
+
+        renderer.render(ctx=context, document=document)
+
+        renderer.env.get_template.assert_called_once_with(document.template)
+        render_func.assert_called_once_with(ctx=context, document=document)
+
+    def test_render_template_not_found(self, site):
+        document = mock.Mock('document')
+        document.template = '/tmp/wrong/path/wrong_template.html'
+        document.document_path = '/tmp/some/document/path.md'
+        context = mock.Mock('context')
+
+        renderer = template.Jinja2Renderer(site=site)
+
+        with pytest.raises(exceptions.RenderingError) as excinfo:
+            renderer.render(ctx=context, document=document)
+
+        assert (f'Template file `{document.template}` for document '
+                f'`{document.document_path}` not found') in str(excinfo.value)
